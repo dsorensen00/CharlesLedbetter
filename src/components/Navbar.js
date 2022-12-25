25f137baf142362592ef7f733fc427a00b693299
Damon Sorensen <damonsorensen@gmail.com>
2022-12-25 09:48:53 -0700

    cleanup for charles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import { React } from "react";
 import { useLocation } from "react-router-dom";
+import EMSSTAR from "../assets/EMS_STAR.png"
 
 export default function Navbar() {
   const location = useLocation();
@@ -7,8 +8,9 @@ export default function Navbar() {
   return (
     <nav class="navbar navbar-expand-lg sticky-top navbar-dark bg-primary">
       <div class="container-fluid">
-        <a class="navbar-brand" href="/">
-          Ledbetter COVID Testing
+        <a class="navbar-brand row" href="/">
+          <img src={EMSSTAR} alt="EMS Star Logo" className="col-1" style={{width:"5%"}}/>
+          First Response Medical
         </a>
         {location.pathname !== "/" ? (
           <div class="collapse navbar-collapse" id="navbarNavDropdown">
--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -2,27 +2,24 @@ import React from "react";
 import OrderConfirmation from "../Checkout/components/OrderConfirmation";
 import BannerNavbar from "./components/BannerNavbar";
 import ShortAboutMe from "./components/ShortAboutMe";
+import EMSSTAR from "../../assets/EMS_STAR.png"
 
 export default function HomePage() {
   return (
     <>
-      {/* <div class="row justify-content-around">
-        <div class="fill col-10">
-          <img src={TestImage} />
+      <div className="container">
+        <div className="row">
+          <img src={EMSSTAR} className="col-4"/>
+            <div className="">
+              <div className="row home-company-title">
+                First Response Medical
+              </div>
+              <div className="row">
+                lalal
+              </div>
+            </div>
+            
         </div>
-      </div> */}
-      <div className="home-heading">
-        <div class="home-heading">
-          <h1>COVID-19</h1>
-          <h1>COVID-19</h1>
-        </div>
-        <p>
-          Testing solutions you can trust.
-          <br />
-          <a href="/checkout">
-            <button class="btn btn-outline-danger btn-small">Get Tested</button>
-          </a>
-        </p>
       </div>
       <div class="container">
         <BannerNavbar />
